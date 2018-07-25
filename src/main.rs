14e811fb4a6d46316dd9f3d966a67472e1fcb07c
Scetch <lucierbrandon@gmail.com>
2018-07-25 12:22:30 -0400

    Check permissions, allow semester selection.

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,45 @@
 extern crate failure;
 #[macro_use] extern crate failure_derive;
+extern crate flexi_logger;
 extern crate itertools;
+#[macro_use] extern crate lazy_static;
+#[macro_use] extern crate log;
 extern crate rayon;
+extern crate regex;
 extern crate reqwest;
 extern crate select;
 extern crate serenity;
-extern crate typemap;
 extern crate tantivy;
-#[macro_use] extern crate log;
-extern crate flexi_logger;
+extern crate typemap;
 
 use std::{ fs, thread };
 
 use failure::{ Error, SyncFailure };
 use itertools::Itertools;
+use regex::Regex;
 use serenity::{
+    CACHE,
     prelude::*,
     model::{
         channel::Message,
         gateway::Game,
         guild::Member,
-        id::ChannelId
+        id::ChannelId,
+        permissions::Permissions,
     },
 };
 
 static IMAGE_DATA: &[u8] = include_bytes!("../uw_logo.png");
 const EMBED_COLOR: u32 = 0x00005696;
+const DEFAULT_TERM: &str = "20185";
+
+lazy_static! {
+    static ref REGEX: Regex = Regex::new(r"([fsw])(\d\d)").unwrap();
+}
 
 mod uwin;
 
 fn main() {
-    // Setup logger.
     flexi_logger::Logger::with_str("uwinsearch")
         .start()
         .expect("Couldn't initialize logger.");
@@ -57,52 +66,92 @@ fn main() {
 struct Handler;
 
 impl Handler {
-    fn fetch_course(&self, ctx: Context, q: &str, chan: ChannelId) -> Result<(), Error> {
-        if q.is_empty() {
-            chan.send_message(|m| {
-                m.embed(|e| {
-                    e.color(EMBED_COLOR)
-                        .field("Usage", "~course <query>", false)
-                        .field("Examples", "~course 60100\n~course calculus", false)
-                })
-            }).map_err(SyncFailure::new)?;
 
-            return Ok(())
-        }
+    fn fetch_course<'a, A>(&self, ctx: Context, mut args: A, chan: ChannelId) -> Result<(), Error>
+        where A: Iterator<Item = &'a str>
+    {
+        let (term, query) = match args.next() {
+            Some("-h") => {
+                chan.send_message(|m| m.embed(|e| {
+                        e.color(EMBED_COLOR)
+                            .field("Usage", "~course [OPTION] <QUERY>", false)
+                            .field("Options", "`-h` View the command help.\n`-s <[fsw]XX>` Select a semester where f (Fall) s (Summer) w (Winter) and XX is the year", false)
+                            .field("Examples", "~course 60100\n~course graph theory\n~course -s f18 graph theory", false)
+                    }))
+                    .map_err(SyncFailure::new)?;
 
-        // Make the bot seem like it's typing a message just in case the query
-        // takes longer than expected
-        chan.broadcast_typing()
-            .map_err(SyncFailure::new)?;
+                return Ok(());
+            }
+            Some("-s") => {
+                // Term codes are in the form [YEAR][CODE] where year is XXXX and
+                // code is 1 (Winder) 2 (Summer) or 5 (Fall)
+                // The bot will allow a user to enter [wWsSfF]XX
+                let term = args.next()
+                    .and_then(|s| REGEX.captures(s))
+                    .and_then(|c| {
+                        let term = match c.get(1)?.as_str() {
+                            "w" | "W" => 1,
+                            "s" | "S" => 2,
+                            "f" | "F" => 5,
+                            _ => return None,
+                        };
+                        let year = c.get(2)?.as_str();
+                        Some(format!("20{}{}", year, term))
+                    });
 
-        // Search the index and get the information from the cache.
-
-        let data = ctx.data.lock();
-
-        // We'll only index if the course index exists. It can be missing if
-        // we are reindexing courses.
-        let index = if let Some(index) = data.get::<uwin::CourseIndex>() {
-            index
-        } else {
-            return Ok(());
-        };
-
-        // TODO: Support selecting terms/semesters
-        let mut courses = match index.query("20185", q) {
-            Ok(courses) => courses,
-            Err(e) => {
-                if let Some(e) = e.downcast_ref::<uwin::QueryError>() {
+                if let Some(term) = term {
+                    (term, args.join(" "))
+                } else {
                     chan.send_message(|m| {
-                            m.content(format!("Query `{}` is invalid.", q))
+                            m.content("Semester selection is invalid.")
                         })
                         .map_err(SyncFailure::new)?;
 
-                    warn!("{}", e);
-
                     return Ok(());
                 }
+            }
+            s => {
+                // Because we popped off the first term attempting to match a
+                // command we have to make sure to join that back with the
+                // rest of the arguments for the query.
+                let query = s.into_iter()
+                    .chain(args)
+                    .join(" ");
 
-                return Err(e);
+                (DEFAULT_TERM.to_owned(), query)
+            }
+        };
+
+        // The course index may not exist if we are reindexing.
+        let data = ctx.data.lock();
+        let index = match data.get::<uwin::CourseIndex>() {
+            Some(index) => index,
+            _ => return Ok(()),
+        };
+
+        // Make the bot seem like it's typing just in case this query
+        // takes longer than expected.
+        chan.broadcast_typing()
+            .map_err(SyncFailure::new)?;
+
+        let mut courses = match index.query(&term, &query) {
+            Ok(courses) => courses,
+            Err(e) => {
+                return match e.downcast::<uwin::QueryError>() {
+                    Ok(e) => {
+                        // If the error is a query error we want to send a message in chat
+                        // telling the user the query was invalid.
+                        warn!("{}", e);
+
+                        chan.send_message(|m| {
+                                m.content(&format_args!("Query `\"{}\"` is invalid.", query))
+                            })
+                            .map_err(SyncFailure::new)?;
+
+                        Ok(())
+                    }
+                    Err(e) => Err(e),
+                };
             }
         };
 
@@ -112,7 +161,7 @@ impl Handler {
         match courses.as_slice() {
             [] => {
                 chan.send_message(|m| {
-                        m.content(format!("No course found for query `{}`.", q))
+                        m.content(format!("No course found for query `\"{}\"`.", query))
                     })
                     .map_err(SyncFailure::new)?;
             }
@@ -214,13 +263,13 @@ impl Handler {
                         f(&format_args!("`{}` {}", course.code, course.title))
                     });
 
-                chan.send_message(|m| {
-                        m.embed(|e| {
-                            e.color(EMBED_COLOR)
-                                .title("Top 10 Results")
-                                .description(courses)
-                        })
-                    })
+                let files = vec![(IMAGE_DATA, "icon.png")];
+                chan.send_files(files, |m| m.embed(|e| {
+                        e.color(EMBED_COLOR)
+                            .thumbnail("attachment://icon.png")
+                            .title("Top 10 Results")
+                            .description(courses)
+                    }))
                     .map_err(SyncFailure::new)?;
             }
         }
@@ -274,21 +323,29 @@ impl Handler {
 
 impl EventHandler for Handler {
     fn message(&self, ctx: Context, msg: Message) {
-        let mut parts = msg.content
+        // Make sure we can send messages in this channel.
+        let can_send = msg.channel()
+            .and_then(|c| c.guild())
+            .map(|c| {
+                let current_user = CACHE.read().user.id;
+                c.read()
+                    .permissions_for(current_user)
+                    .map(|p| p.contains(Permissions::SEND_MESSAGES | Permissions::ATTACH_FILES))
+                    .unwrap_or(false)
+            })
+            .unwrap_or(true);
+
+        if !can_send {
+            return;
+        }
+
+        let mut args = msg.content
             .split_whitespace();
 
-        let cmd = if let Some(cmd) = parts.next() {
-            cmd
-        } else {
-            return;
-        };
-
-        let args = parts.join(" ");
-
-        let cmd = match cmd {
-            "~course" => self.fetch_course(ctx, &args, msg.channel_id),
-            "~reindex" => self.reindex(ctx, msg.member()),
-            _ => Ok(()),
+        let cmd = match args.next() {
+            Some("~course") => self.fetch_course(ctx, args, msg.channel_id),
+            Some("~reindex") => self.reindex(ctx, msg.member()),
+            _ => return,
         };
 
         if let Err(e) = cmd {
